318e1dca464bf4ee70ff79c98e6526f90b7fa83c
50kbefore25coming <nguyenquyquyenn@gmail.com>
2025-09-14 19:56:00 +0700

    k có gì

--- VScode-Python/ThucHanh.py
+++ VScode-Python/ThucHanh.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import math
+MAX = 10**7
+Mang = [0] * (MAX + 1)
+for i in range(1, MAX + 1):
+    for j in range(i, MAX + 1, i):
+        Mang[j] += 1
 
-l, r = map(int, input().split())
-for a in range(l, r - 1):
-    for b in range(a + 1, r):
-        if math.gcd(a, b) == 1:
-            for c in range(b + 1, r + 1):
-                if math.gcd(a, c) == 1 and math.gcd(b, c) == 1:
-                    print(f"({a}, {b}, {c})")
-=======
-arr = [10, 20, 30]
-arr[1] = 99       # thay phần tử thứ 2 (20 → 99)
-print(arr)        # [10, 99, 30]
->>>>>>> c1cc094 (8/9 2 tiếng)
+print(Mang[50])
